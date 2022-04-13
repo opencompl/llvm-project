3d0b76623923b7271aeb8c61adcdd9d9930e1cba
Arjun P <arjunpitchanathan@gmail.com>
2022-04-13 17:24:15 +0100

    [MLIR][Prebsurger] introduce TPInt, an auto-resizing APInt for fully correct signed integer computations Summary: The Presburger library currently uses int64_t throughout for its integers. This runs the risk of silently producing incorrect results when overflows occur. Fixing this issues requires some sort of multiprecision integer that transparently supports aribtrary arithmetic computations.
    Since this is based on APInt, it optimizes for the common case of small integers
    by using a 64-bit machine integer to represent the integer when possible.
    
    Reviewers: ftynse, bondhugula, Groverkss
    
    Subscribers: grosser

---
+++ mlir/include/mlir/Analysis/Presburger/TPInt.h
@@ -0,0 +1,138 @@
+//===- TPInt.h - MLIR TPInt Class -------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This is a simple class to represent arbitrary precision signed integers.
+// Unlike APInt, one does not have to specify a fixed maximum size, and the
+// integer can take on any aribtrary values.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_ANALYSIS_PRESBURGER_TPINT_H
+#define MLIR_ANALYSIS_PRESBURGER_TPINT_H
+
+#include "llvm/ADT/APSInt.h"
+#include "mlir/Support/MathExtras.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace mlir {
+namespace presburger {
+
+/// This class provides support for multi-precision arithmetic.
+/// 
+/// Unlike APInt, this extends the precision as necessary to prevent overflows
+/// and supports operations between objects with differing internal precisions.
+/// 
+/// Since it uses APInt internally, TPInt (TransPrecision Int) stores values in
+/// a 64-bit machine integer internally for small values and uses slower
+/// arbitrary-precision arithmetic only for larger values.
+class TPInt {
+public:
+  explicit TPInt(int64_t val) : val(APSInt::get(val)) {}
+  TPInt() : TPInt(0) {}
+  explicit TPInt(const APSInt &val) : val(val) {}
+  TPInt &operator=(int64_t val) {
+    return *this = TPInt(val);
+  }
+  explicit operator int64_t() const {
+    return val.getSExtValue();
+  }
+  TPInt operator-() const;
+  bool operator==(const TPInt &o) const;
+  bool operator!=(const TPInt &o) const;
+  bool operator>(const TPInt &o) const;
+  bool operator<(const TPInt &o) const;
+  bool operator<=(const TPInt &o) const;
+  bool operator>=(const TPInt &o) const;
+  TPInt operator+(const TPInt &o) const;
+  TPInt operator-(const TPInt &o) const;
+  TPInt operator*(const TPInt &o) const;
+  TPInt operator/(const TPInt &o) const;
+  TPInt operator%(const TPInt &o) const;
+  TPInt &operator+=(const TPInt &o);
+  TPInt &operator-=(const TPInt &o);
+  TPInt &operator*=(const TPInt &o);
+  TPInt &operator/=(const TPInt &o);
+  TPInt &operator%=(const TPInt &o);
+
+  TPInt &operator++();
+  TPInt &operator--();
+
+  friend TPInt abs(const TPInt &x);
+  friend TPInt ceilDiv(const TPInt &lhs, const TPInt &rhs);
+  friend TPInt floorDiv(const TPInt &lhs, const TPInt &rhs);
+  friend TPInt greatestCommonDivisor(const TPInt &a, const TPInt &b);
+  /// Overload to compute a hash_code for a TPInt value.
+  friend llvm::hash_code hash_value(const TPInt &x); // NOLINT
+
+  llvm::raw_ostream &print(llvm::raw_ostream &os) const;
+  void dump() const;
+
+private:
+  unsigned getBitWidth() const {
+    return val.getBitWidth();
+  }
+
+  // The held integer value.
+  // 
+  // TODO: consider using APInt directly to avoid unnecessary repeated internal
+  // signedness checks. This requires refactoring, exposing, or duplicating
+  // APSInt::compareValues.
+  APSInt val;
+};
+
+/// This just calls through to the operator int64_t, but it's useful when a
+/// function pointer is required.
+inline int64_t int64FromTPInt(const TPInt &x) {
+  return int64_t(x);
+}
+
+llvm::raw_ostream &operator<<(llvm::raw_ostream &os, const TPInt &x);
+
+// The RHS is always expected to be positive, and the result
+/// is always non-negative.
+TPInt mod(const TPInt &lhs, const TPInt &rhs);
+
+/// Returns the least common multiple of 'a' and 'b'.
+TPInt lcm(const TPInt &a, const TPInt &b);
+
+/// Convenience overloads for 64-bit integers.
+TPInt &operator+=(TPInt &a, int64_t b);
+TPInt &operator-=(TPInt &a, int64_t b);
+TPInt &operator*=(TPInt &a, int64_t b);
+TPInt &operator/=(TPInt &a, int64_t b);
+TPInt &operator%=(TPInt &a, int64_t b);
+
+bool operator==(const TPInt &a, int64_t b);
+bool operator!=(const TPInt &a, int64_t b);
+bool operator>(const TPInt &a, int64_t b);
+bool operator<(const TPInt &a, int64_t b);
+bool operator<=(const TPInt &a, int64_t b);
+bool operator>=(const TPInt &a, int64_t b);
+TPInt operator+(const TPInt &a, int64_t b);
+TPInt operator-(const TPInt &a, int64_t b);
+TPInt operator*(const TPInt &a, int64_t b);
+TPInt operator/(const TPInt &a, int64_t b);
+TPInt operator%(const TPInt &a, int64_t b);
+
+bool operator==(int64_t a, const TPInt &b);
+bool operator!=(int64_t a, const TPInt &b);
+bool operator>(int64_t a, const TPInt &b);
+bool operator<(int64_t a, const TPInt &b);
+bool operator<=(int64_t a, const TPInt &b);
+bool operator>=(int64_t a, const TPInt &b);
+TPInt operator+(int64_t a, const TPInt &b);
+TPInt operator-(int64_t a, const TPInt &b);
+TPInt operator*(int64_t a, const TPInt &b);
+TPInt operator/(int64_t a, const TPInt &b);
+TPInt operator%(int64_t a, const TPInt &b);
+
+} // namespace presburger
+} // namespace mlir
+
+#endif // MLIR_ANALYSIS_PRESBURGER_TPINT_H
+
---
+++ mlir/lib/Analysis/Presburger/TPInt.cpp
@@ -0,0 +1,268 @@
+//===- TPInt.cpp - MLIR TPInt Class ---------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Analysis/Presburger/TPInt.h"
+#include "llvm/Support/MathExtras.h"
+
+using namespace mlir;
+using namespace presburger;
+
+llvm::hash_code mlir::presburger::hash_value(const TPInt &x) {
+  return hash_value(x.val);
+}
+
+
+/// ---------------------------------------------------------------------------
+/// Printing.
+/// ---------------------------------------------------------------------------
+llvm::raw_ostream &TPInt::print(llvm::raw_ostream &os) const {
+  return os << val;
+}
+
+void TPInt::dump() const {
+  print(llvm::errs());
+}
+
+llvm::raw_ostream &mlir::presburger::operator<<(llvm::raw_ostream &os, const TPInt &x) {
+  x.print(os);
+  return os;
+}
+
+/// ---------------------------------------------------------------------------
+/// Comparison operators.
+/// ---------------------------------------------------------------------------
+bool TPInt::operator==(const TPInt &o) const {
+  return APSInt::compareValues(val, o.val) == 0;
+}
+bool TPInt::operator!=(const TPInt &o) const {
+  return APSInt::compareValues(val, o.val) != 0;
+}
+bool TPInt::operator>(const TPInt &o) const {
+  return APSInt::compareValues(val, o.val) > 0;
+}
+bool TPInt::operator<(const TPInt &o) const {
+  return APSInt::compareValues(val, o.val) < 0;
+}
+bool TPInt::operator<=(const TPInt &o) const {
+  return APSInt::compareValues(val, o.val) <= 0;
+}
+bool TPInt::operator>=(const TPInt &o) const {
+  return APSInt::compareValues(val, o.val) >= 0;
+}
+
+/// ---------------------------------------------------------------------------
+/// Arithmetic operators.
+/// ---------------------------------------------------------------------------
+using APIntOvOp =  APInt (APInt::*)(const APInt &b, bool &overflow) const;
+
+/// Bring a and b to have the same width and then call a.op(b, overflow).
+/// If the overflow bit becomes set, resize a and b to double the width and
+/// call a.op(b, overflow), returning its result. The operation with double
+/// widths should not also overflow.
+APSInt doOpExpandIfOverflow(const APInt &a, const APInt &b, APIntOvOp op) {
+  bool overflow;
+  unsigned width = std::max(a.getBitWidth(), b.getBitWidth());
+  // This calls a.sextOrSelf(width).op(b.sextOrSelf(width), overflow).
+  // TODO: in C++17 we can use the simpler syntax with std::invoke.
+  APInt ret = ((a.sextOrSelf(width)).*(op))(b.sextOrSelf(width), overflow);
+  if (!overflow)
+    return APSInt(ret, /*isUnsigned=*/false);
+
+  width *= 2;
+  // This calls a.sextOrSelf(width).op(b.sextOrSelf(width), overflow).
+  ret = ((a.sextOrSelf(width)).*(op))(b.sextOrSelf(width), overflow);
+  assert(!overflow && "double width should be sufficient to avoid overflow!");
+  return APSInt(ret, /*isUnsigned=*/false);
+}
+
+TPInt TPInt::operator+(const TPInt &o) const {
+  return TPInt(doOpExpandIfOverflow(val, o.val, &APInt::sadd_ov));
+}
+TPInt TPInt::operator-(const TPInt &o) const {
+  return TPInt(doOpExpandIfOverflow(val, o.val, &APInt::ssub_ov));
+}
+TPInt TPInt::operator*(const TPInt &o) const {
+  return TPInt(doOpExpandIfOverflow(val, o.val, &APInt::smul_ov));
+}
+TPInt TPInt::operator/(const TPInt &o) const {
+  return TPInt(doOpExpandIfOverflow(val, o.val, &APInt::sdiv_ov));
+}
+namespace mlir {
+namespace presburger {
+using llvm::APIntOps::RoundingSDiv;
+using llvm::APIntOps::GreatestCommonDivisor;
+TPInt abs(const TPInt &x) {
+  return x >= 0 ? x : -x;
+}
+TPInt ceilDiv(const TPInt &lhs, const TPInt &rhs) {
+  if (rhs == -1)
+    return -lhs;
+  return TPInt(APSInt(RoundingSDiv(lhs.val, rhs.val, APInt::Rounding::UP), /*isUnsigned=*/false));
+}
+TPInt floorDiv(const TPInt &lhs, const TPInt &rhs) {
+  if (rhs == -1)
+    return -lhs;
+  return TPInt(APSInt(RoundingSDiv(lhs.val, rhs.val, APInt::Rounding::DOWN), /*isUnsigned=*/false));
+}
+// The RHS is always expected to be positive, and the result
+/// is always non-negative.
+TPInt mod(const TPInt &lhs, const TPInt &rhs) {
+  assert(rhs >= 1);
+  return lhs % rhs < 0 ? lhs % rhs + rhs : lhs % rhs;
+}
+
+TPInt greatestCommonDivisor(const TPInt &a, const TPInt &b) {
+  return TPInt(APSInt(GreatestCommonDivisor(a.val.abs(), b.val.abs()), /*isUnsigned=*/false));
+}
+
+/// Returns the least common multiple of 'a' and 'b'.
+TPInt lcm(const TPInt &a, const TPInt &b) {
+  TPInt x = abs(a);
+  TPInt y = abs(b);
+  TPInt lcm = (x * y) / greatestCommonDivisor(x, y);
+  assert((lcm >= a && lcm >= b) && "LCM overflow");
+  return lcm;
+}
+} // namespace presburger
+} // namespace mlir
+
+TPInt TPInt::operator%(const TPInt &o) const {
+  unsigned width = std::max(val.getBitWidth(), o.val.getBitWidth());
+  return TPInt(APSInt(val.sextOrSelf(width).srem(o.val.sextOrSelf(width)), /*isUnsigned=*/false));
+}
+
+TPInt TPInt::operator-() const {
+  if (val.isMinSignedValue()) {
+    APSInt ret = val.extend(2*val.getBitWidth());
+    return TPInt(-ret);
+  }
+  return TPInt(-val);
+}
+
+/// ---------------------------------------------------------------------------
+/// Assignment operators, preincrement, predecrement.
+/// ---------------------------------------------------------------------------
+TPInt &TPInt::operator+=(const TPInt &o) {
+  *this = *this + o;
+  return *this;
+}
+TPInt &TPInt::operator-=(const TPInt &o) {
+  *this = *this - o;
+  return *this;
+}
+TPInt &TPInt::operator*=(const TPInt &o) {
+  *this = *this * o;
+  return *this;
+}
+TPInt &TPInt::operator/=(const TPInt &o) {
+  *this = *this / o;
+  return *this;
+}
+TPInt &TPInt::operator%=(const TPInt &o) {
+  *this = *this % o;
+  return *this;
+}
+TPInt &TPInt::operator++() {
+  *this += 1;
+  return *this;
+}
+
+TPInt &TPInt::operator--() {
+  *this -= 1;
+  return *this;
+}
+
+/// ---------------------------------------------------------------------------
+/// Convenience operator overloads for int64_t.
+/// ---------------------------------------------------------------------------
+namespace mlir {
+namespace presburger {
+TPInt &operator+=(TPInt &a, int64_t b) {
+  return a += TPInt(b);
+}
+TPInt &operator-=(TPInt &a, int64_t b) {
+  return a -= TPInt(b);
+}
+TPInt &operator*=(TPInt &a, int64_t b) {
+  return a *= TPInt(b);
+}
+TPInt &operator/=(TPInt &a, int64_t b) {
+  return a /= TPInt(b);
+}
+TPInt &operator%=(TPInt &a, int64_t b) {
+  return a %= TPInt(b);
+}
+bool operator==(const TPInt &a, int64_t b) {
+  return a == TPInt(b);
+}
+bool operator!=(const TPInt &a, int64_t b) {
+  return a != TPInt(b);
+}
+bool operator>(const TPInt &a, int64_t b) {
+  return a > TPInt(b);
+}
+bool operator<(const TPInt &a, int64_t b) {
+  return a < TPInt(b);
+}
+bool operator<=(const TPInt &a, int64_t b) {
+  return a <= TPInt(b);
+}
+bool operator>=(const TPInt &a, int64_t b) {
+  return a >= TPInt(b);
+}
+TPInt operator+(const TPInt &a, int64_t b) {
+  return a + TPInt(b);
+}
+TPInt operator-(const TPInt &a, int64_t b) {
+  return a - TPInt(b);
+}
+TPInt operator*(const TPInt &a, int64_t b) {
+  return a * TPInt(b);
+}
+TPInt operator/(const TPInt &a, int64_t b) {
+  return a / TPInt(b);
+}
+TPInt operator%(const TPInt &a, int64_t b) {
+  return a % TPInt(b);
+}
+bool operator==(int64_t a, const TPInt &b) {
+  return TPInt(a) == b;
+}
+bool operator!=(int64_t a, const TPInt &b) {
+  return TPInt(a) != b;
+}
+bool operator>(int64_t a, const TPInt &b) {
+  return TPInt(a) > b;
+}
+bool operator<(int64_t a, const TPInt &b) {
+  return TPInt(a) < b;
+}
+bool operator<=(int64_t a, const TPInt &b) {
+  return TPInt(a) <= b;
+}
+bool operator>=(int64_t a, const TPInt &b) {
+  return TPInt(a) >= b;
+}
+TPInt operator+(int64_t a, const TPInt &b) {
+  return TPInt(a) + b;
+}
+TPInt operator-(int64_t a, const TPInt &b) {
+  return TPInt(a) - b;
+}
+TPInt operator*(int64_t a, const TPInt &b) {
+  return TPInt(a) * b;
+}
+TPInt operator/(int64_t a, const TPInt &b) {
+  return TPInt(a) / b;
+}
+TPInt operator%(int64_t a, const TPInt &b) {
+  return TPInt(a) % b;
+}
+} // namespace presburger
+} // namespace mlir
+
