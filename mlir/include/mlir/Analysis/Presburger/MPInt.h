22e96e3906294f3afb557cdd109990c4a57a93fd
Arjun P <arjunpitchanathan@gmail.com>
2022-04-23 03:43:47 +0100

    %, unary - [.38]

--- mlir/include/mlir/Analysis/Presburger/MPInt.h
+++ mlir/include/mlir/Analysis/Presburger/MPInt.h
@@ -327,16 +327,16 @@ inline MPInt lcm(const MPInt &a, const MPInt &b) {
 inline MPInt MPInt::operator%(const MPInt &o) const {
   if (isSmall() && o.isSmall())
     return MPInt(get64() % o.get64());
-  abort();
+  return MPInt(getAsAP() % o.getAsAP());
 }
 
 inline MPInt MPInt::operator-() const {
   if (isSmall()) {
     if (get64() != std::numeric_limits<int64_t>::min())
       return MPInt(-get64());
-    abort();
+    return MPInt(-getAsAP());
   }
-  abort();
+  return MPInt(-getAsAP());
 }
 
 /// ---------------------------------------------------------------------------
