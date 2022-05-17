f56e5359db77d5cf765e2db5134ab7e2df7538be
Arjun P <arjunpitchanathan@gmail.com>
2022-05-17 10:17:15 +0100

    add microbenchmark

---
+++ mlir/benchmarks/mpint.bench.cpp
@@ -0,0 +1,66 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Analysis/Presburger/MPInt.h"
+#include <cstdint>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "benchmark/benchmark.h"
+
+#define N (1024 * 16)
+
+using namespace mlir::presburger;
+
+static void mulI64(benchmark::State &state) {
+  long a[N];
+  long b[N];
+  long c[N];
+  for (int i = 0; i < N; i++) {
+	a[i] = i;
+	b[i] = i;
+  }
+
+  for (auto _ : state)
+  // #pragma nounroll
+ 	for (int i = 0; i < N; i+=16) {
+    // __sync_synchronize();
+    c[i] = a[i] * b[i];
+	}
+
+  benchmark::DoNotOptimize(c[42]);
+}
+BENCHMARK(mulI64);
+
+static void mulMpint(benchmark::State &state) {
+  MPInt a[N];
+  MPInt b[N];
+  MPInt c[N];
+  for (int i = 0; i < N; i++) {
+	a[i] = i;
+	b[i] = i;
+  }
+
+  for (auto _ : state)
+  // #pragma nounroll
+ 	for (int i = 0; i < N; i+=16) {
+	    c[i] = a[i] * b[i];
+	}
+
+  benchmark::DoNotOptimize(c[42]);
+}
+BENCHMARK(mulMpint);
+
+int main(int argc, char** argv) {
+  benchmark::Initialize(&argc, argv);
+  if (benchmark::ReportUnrecognizedArguments(argc, argv))
+    return 1;
+
+  benchmark::RunSpecifiedBenchmarks();
+}
