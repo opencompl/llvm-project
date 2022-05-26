c40b1d1241aa0f2613833b7679174790944dd236
Arjun P <arjunpitchanathan@gmail.com>
2022-05-26 18:02:55 +0100

    remove incorrect and unnecessary lcm fastpath

--- mlir/include/mlir/Analysis/Presburger/MPInt.h
+++ mlir/include/mlir/Analysis/Presburger/MPInt.h
@@ -336,11 +336,6 @@ inline MPInt gcd(const MPInt &a, const MPInt &b) {
 
 /// Returns the least common multiple of 'a' and 'b'.
 inline MPInt lcm(const MPInt &a, const MPInt &b) {
-  if (LLVM_LIKELY(a.isSmall() && b.isSmall())) {
-    int64_t absA = std::abs(a.get64());
-    int64_t absB = std::abs(b.get64());
-    return MPInt((absA * absB) / llvm::GreatestCommonDivisor64(absA, absB));
-  }
   MPInt x = abs(a);
   MPInt y = abs(b);
   return (x * y) / gcd(x, y);
