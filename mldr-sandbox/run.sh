ae4f5e193626f35733f7977d9e19324955a56b07
Ivan Ho <ihkh2@cam.ac.uk>
2025-04-24 15:02:02 +0000

    clean up snapshot code

--- mldr-sandbox/run.sh
+++ mldr-sandbox/run.sh
@@ -3,4 +3,7 @@ BUILD_DIR=../build
 alias mlir-opt=${BUILD_DIR}/bin/mlir-opt
 
 cmake --build ${BUILD_DIR} &&
-  mlir-opt -mlir-print-debuginfo --pass-pipeline="builtin.module(test-constant-mul, snapshot-op-locations{print-debuginfo=1 pretty-debuginfo=1 filename=snapshot.mlir})" test.mlir
+  mlir-opt -mlir-print-debuginfo --pass-pipeline="builtin.module\
+    ( test-constant-mul \
+    , snapshot-op-locations{print-debuginfo=1 pretty-debuginfo=1 filename=snapshot-0.mlir} \
+    )" test.mlir > snapshot-out.mlir
