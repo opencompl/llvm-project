9db233d53c014e7b845997631bf66c99211dbc0f
George Rennie <georgerennie@gmail.com>
2025-11-06 13:24:41 +0000

    buffed: typo

--- utils/build-llvm.sh
+++ utils/build-llvm.sh
@@ -44,4 +44,4 @@ cmake -G Ninja ../llvm \
   -DLLVM_ENABLE_ASSERTIONS=ON \
   -DMLIR_INCLUDE_INTEGRATION_TESTS=ON
 
-ninja install test-buffed test-asymptotics
+ninja install test-buffed test-asymptotic
