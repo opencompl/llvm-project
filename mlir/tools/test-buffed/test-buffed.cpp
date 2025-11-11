e78413fd512a5acacc38dfd8197f6c80602b401e
George Rennie <georgerennie@gmail.com>
2025-11-11 02:31:16 +0000

    Add add-zero benchmark

--- mlir/tools/test-buffed/test-buffed.cpp
+++ mlir/tools/test-buffed/test-buffed.cpp
@@ -21,7 +21,7 @@
 using namespace llvm;
 using namespace mlir;
 
-Operation *matchAndRewrite(Operation *op) {
+static Operation *rewriteAddConstantFolding(Operation *op) {
   auto addOp = dyn_cast<arith::AddIOp>(op);
   if (!addOp)
     return nullptr;
@@ -52,37 +52,62 @@ Operation *matchAndRewrite(Operation *op) {
   return foldCst;
 }
 
-void rewriteModule(ModuleOp module) {
+static Operation *rewriteAddZero(Operation *op) {
+  auto addOp = dyn_cast<arith::AddIOp>(op);
+  if (!addOp)
+    return nullptr;
+  auto lhs = addOp.getLhs();
+  auto rhs = addOp.getRhs();
+
+  auto cst0 = lhs.getDefiningOp<arith::ConstantIntOp>();
+  if (!cst0)
+    return nullptr;
+
+  if (cst0.value() != 0)
+    return nullptr;
+
+  addOp.replaceAllUsesWith(rhs);
+  addOp.erase();
+
+  if (lhs.getUses().empty())
+    lhs.getDefiningOp()->erase();
+
+  return nullptr;
+}
+
+template <typename F>
+static void rewriteModule(ModuleOp module, F f) {
   Operation *op = &module.getBody()->getOperations().front();
   while (op) {
-    auto *newOp = matchAndRewrite(op);
+    Operation* next = op->getNextNode();
+    auto *newOp = f(op);
     if (newOp) {
       op = newOp;
       continue;
     }
-    op = op->getNextNode();
+    op = next;
   }
 }
 
-OwningOpRef<ModuleOp> createModule(MLIRContext &ctx, uint64_t size) {
+static OwningOpRef<ModuleOp> createModule(MLIRContext &ctx, uint64_t size, uint32_t root, uint32_t increment) {
   OpBuilder builder(&ctx);
   OwningOpRef<ModuleOp> module =
       builder.create<ModuleOp>(UnknownLoc::get(&ctx));
   builder = builder.atBlockBegin(module->getBody());
 
   Operation *rootOp = builder.create<arith::ConstantOp>(
-      UnknownLoc::get(&ctx), IntegerAttr::get(IntegerType::get(&ctx, 32), 1));
+      UnknownLoc::get(&ctx), IntegerAttr::get(IntegerType::get(&ctx, 32), root));
   for (uint64_t i = 0; i < size; ++i) {
     auto cst0 = builder.create<arith::ConstantOp>(
-        UnknownLoc::get(&ctx), IntegerAttr::get(IntegerType::get(&ctx, 32), 1));
+        UnknownLoc::get(&ctx), IntegerAttr::get(IntegerType::get(&ctx, 32), increment));
     rootOp = builder.create<arith::AddIOp>(
-        UnknownLoc::get(&ctx), rootOp->getResult(0), cst0->getResult(0));
+        UnknownLoc::get(&ctx), cst0->getResult(0), rootOp->getResult(0));
   }
   return module;
 }
 
 template<typename F>
-auto time(std::string_view name, F f) {
+static auto time(std::string_view name, F f) {
   std::chrono::high_resolution_clock::time_point start =
       std::chrono::high_resolution_clock::now();
 
@@ -120,9 +145,16 @@ int main(int argc, char **argv) {
 
   if (BenchmarkMode == "constant-folding") {
     OwningOpRef<ModuleOp> module = time("create", [&]() {
-        return createModule(ctx, BenchmarkSize);
+        return createModule(ctx, BenchmarkSize, 42, 1);
     });
-    time("rewrite", [&]() { rewriteModule(*module); });
+    time("rewrite", [&]() { rewriteModule(*module, rewriteAddConstantFolding); });
+    module->dump();
+  }
+  else if (BenchmarkMode == "add-zero") {
+    OwningOpRef<ModuleOp> module = time("create", [&]() {
+        return createModule(ctx, BenchmarkSize, 42, 0);
+    });
+    time("rewrite", [&]() { rewriteModule(*module, rewriteAddZero); });
     module->dump();
   } else {
     llvm::errs() << "Unrecognised benchmark name\n";
